afb00222fd6e180f8b195a4100f2261f28406716
Homero Raul <h.raulvc@gmail.com>
2024-03-12 15:42:41 -0600

    Update

--- resources/views/comment.blade.php
+++ resources/views/comment.blade.php
@@ -1,6 +1,19 @@
 <div>
+    @if($role != 'student' || true)
+        <div wire:ignore="" class="row">
+            <div class="col-12">
+                <a 
+                    href="{{ route('forum.report', $blockId) }}" 
+                    type="button" 
+                    class="btn btn-light rounded-0 float-right"
+                    target="_blanck">
+                    Ver reporte de foro
+                </a>
+            </div>
+        </div>
+    @endif
     <div id="commentSystem_Content"></div>
     <script>
-        var commentSystem_ContentID="{{ $blockId }}", commentSystem_Title="{{ $title }}",commentSystem_ContentURL="{{ url()->current() }}?role={{ $role }}&token=pa&callback={{ $callback }}",commentSystem_Language="es",commentSystem_FooterLinks="Off",commentSystem_userid="{{ auth()->id() }}",commentSystem_username="{{ auth()->user()->name }}",commentSystem_usericon="{{ route('user.avatar', auth()->id()) }}",commentSystem_profillink="{{ url('user/' . auth()->id() . '/comments') }}",commentSystem_Domain="https://comment-system.innoboxrr.com";!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src=commentSystem_Domain+"/plugin/embed.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}();
+        var commentSystem_ContentID="{{ $blockId }}", commentSystem_Title="{{ $title }}",commentSystem_ContentURL="{{ url()->current() }}?role={{ $role }}&token=pa&callback={{ $callback }}",commentSystem_Language="es",commentSystem_FooterLinks="Off",commentSystem_userid="{{ auth()->id() }}",commentSystem_username="{{ auth()->user()->name }}",commentSystem_usericon="{{ route('lwc.comments.user.avatar', auth()->id()) }}",commentSystem_profillink="{{ url('user/' . auth()->id() . '/comments') }}",commentSystem_Domain="https://comment-system.innoboxrr.com";!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src=commentSystem_Domain+"/plugin/embed.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}();
     </script>
 </div>
